10a953285cb55722537bf26a1f311cc586bbeddf
Arbabian21 <arifarbab21@gmail.com>
2024-09-19 14:24:01 -0400

    added state and onClick

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+	const [headingText, setHeadingText] = useState("Hello");
+
+	function handleClick() {
+		setHeadingText("Submitted");
+	}
+
+	return (
+		<div className="container">
+			<h1>{headingText}</h1>
+			<input type="text" placeholder="What's your name?" />
+			<button onClick={handleClick}>Submit</button>
+		</div>
+	);
 }
 
 export default App;
