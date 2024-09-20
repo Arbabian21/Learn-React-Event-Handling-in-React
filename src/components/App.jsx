9049fca64dc316174e1ab61f8ec9711fee1acf58
Arbabian21 <arifarbab21@gmail.com>
2024-09-20 12:24:54 -0400

    added state for hovering over submit to toggle style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,32 @@ import React, { useState } from "react";
 
 function App() {
 	const [headingText, setHeadingText] = useState("Hello");
+	const [isMouseOver, setIsMouseOver] = useState(false);
 
 	function handleClick() {
 		setHeadingText("Submitted");
+	}
+
+	function handleMouseOver() {
+		setIsMouseOver(true);
+	}
+
+	function handleMouseOut() {
+		setIsMouseOver(false);
 	}
 
 	return (
 		<div className="container">
 			<h1>{headingText}</h1>
 			<input type="text" placeholder="What's your name?" />
-			<button onClick={handleClick}>Submit</button>
+			<button
+				onClick={handleClick}
+				onMouseOver={handleMouseOver} 
+				onMouseOut={handleMouseOut}
+				style={{ backgroundColor: isMouseOver && "black" }}
+			>
+				Submit
+			</button>
 		</div>
 	);
 }
